ceabbf9c4783115cd60654454100e38b5e63d745
Matt Knapper <mknapper1@gmail.com>
2018-03-30 11:15:32 -0400

    better form labels

--- scout/forms.py
+++ scout/forms.py
@@ -19,6 +19,13 @@ class MatchForm(forms.ModelForm):
     class Meta:
         model = Match
         fields = '__all__'
+        labels = {
+            "scale_cubes": "Scale Cubes",
+            "robot_speed" : "Robot Speed (rank from 1 to 10)",
+            "robot_cube_holding" : "Robot skill at holding cubes(1 to 10) ",
+            "robot_cube_intake" : "Robot skill at intaking cubes (1 to 10)",
+            "robot_cube_placing" : "Robot skill at placing cubes (1 to 10)"
+        }
 
 
 class UploadFileForm(forms.Form):
