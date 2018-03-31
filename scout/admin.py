e5e2258f0780fcb3a0351456b4fbc02774f6b32d
Matt Knapper <mknapper1@gmail.com>
2018-03-31 08:33:14 -0400

    added robotadmin

--- scout/admin.py
+++ scout/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from .models import Robot, Pit, Match
 
 
-admin.site.register(Robot)
+class RobotAdmin(admin.ModelAdmin):
+    list_display = ['robot',
+                    'match_number',
+                    'scale_cubes',
+                    'vault_cubes',
+                    'can_climb',
+                    'created',
+                    'updated']
+    list_filter = ['robot', 'match_number']
+
+
+admin.site.register(Robot, RobotAdmin)
 admin.site.register(Pit)
 admin.site.register(Match)
